e5059d04a6612b0886f5dd3ef29df5b13db6af4c
rafin8990 <rafin8990@gmail.com>
2023-10-01 00:09:13 +0600

    project setup done and dependency successfully installed

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: Response) => {
+  res.send('Food fiesta applicationh running succesfully ')
+})
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('database connected successfully')
+    app.listen(config.port, () => {
+      console.log(`Food Fiesta project running on port ${config.port}`)
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
